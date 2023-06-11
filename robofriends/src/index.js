95ad91975d4f14aafa68cef1224bbff39e62f6cc
iluviien <stefanica695@gmail.com>
2023-06-11 13:00:24 +0000

    adding progress

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Hello from "./Hello.js";
+import Card from "./Card.js";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
+import { robots } from "./robots.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Hello greeting={"Hello" + "React Ninja"} />);
+root.render(
+  <div>
+    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+  </div>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
